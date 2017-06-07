a51b45cc014180dcd722be78dadf10a3f74f790a
Kretic <xkretic@gmail.com>
2017-06-07 20:17:43 +0200

    mod picked up in mc

---
+++ src/main/java/kretic/potentiallamp/PotentialLamp.java
@@ -0,0 +1,8 @@
+package kretic.potentiallamp;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid="potentiallamp", name="Potential Lamp", version = "1.11.2-1.0")
+public class PotentialLamp
+{
+}
